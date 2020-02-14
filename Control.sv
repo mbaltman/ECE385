279089b95b4d6b5d8b3ef272b495d4784bc11efd
mbaltman <mbaltman@cps.edu>
2020-02-14 17:19:16 -0600

    First Pass, basic setup
    Copied over Sycnhronizer an dHEx Driver from 8bit logic processor,
    Created Procesor and Control. control is not finished yet

---
+++ Control.sv
@@ -0,0 +1,74 @@
+module control(input logic Clk, Reset, Run, clra_ldb,b0
+					output logic OutCleara, OutLoadb, OutShift,OutAdd,OutSub);
+	//these are the states
+	enum logic [4:0] 	{Start, Clear,Halt,
+							Add1, Add2, Add3, Add4,Add5,Add6,Add7,Sub8
+							Shift1, Shift2, Shift3, Shift4, Shift4, Shift5,Shift6,Shift7,Shift8}
+							curr_state, next_state;
+							
+	//updates flip flop, Current state is the only one
+	always_ff @ (posedge Clk)  
+    begin
+        if (Reset)
+            curr_state <= Start;
+        else 
+            curr_state <= next_state;
+    end
+	 
+	 always_comb
+	 begin
+		next_state  = curr_state;
+		
+		unique case(curr_state)
+			
+			Start: if(Run)
+							next_state = Clear;
+			Clear: if(b0)
+							next_state = Add1;
+					 else
+							next_state = Shift1;			
+			Add1: next_state= Shift1;
+			
+			Shift1: if(b0)
+							next_state = Add2;
+					  else
+							next_state = Shift2;
+							
+			Add2: next_state = Shift2;
+			
+			Shift2: if(b0)
+							next_state = Add3;
+					  else
+							next_state = Shift3;
+			Add3: next_state = Shift3;
+			
+			Shift3: if(b0)
+							next_state = Add4;
+					  else
+							next_state = Shift4;
+			Add4: next_state = Shift4;
+			
+			Shift4: if(b0)
+							next_state = Add5;
+					  else
+							next_state = Shift5;
+			Add5: next_state = Shift5;
+			
+			Shift5: if(b0)
+							next_state = Add6;
+					  else
+							next_state = Shift6;
+			Add6: next_state = Shift7;
+			
+			Shift7: if(b0)
+							next_state = Sub8;
+					  else 
+							next_state = Shift8;
+			Sub8: next_state = Shift8;
+			Shift8: next_state = Halt;
+			Halt: if(~Execute)
+						next_state = Start;
+		
+	 end
+				
+endmodule
---
+++ Processor.sv
@@ -0,0 +1,48 @@
+/* This module is the processor for multiplier, and it does all the wiring and instatiation. */
+module processor
+	(
+  input logic clk, reset, run, cleara_loadb,
+  input logic[7:0] s,
+  output logic[6:0] AhexU, AhexL, BhexU, BhexL,
+  output logic[7:0] aval, bval,
+  output logic x
+	);
+
+/* Intermediate logic variables go here. */
+  logic shift, add, sum, cleara,loadb;
+  logic run_s, cleara_loadb_s;
+  logic [7:0] s_s,a,b;
+
+/* Instantiation of other modules here. */
+  // two 8-bit shift registers
+  register_unit reg_a(.clk_reg(clk),.reset_reg(reset),.shift_in_reg(x),.shift_en_reg(),.din_reg(s_s),.shift_out_reg(),.dout_reg(a));
+  register_unit reg_b();
+  
+	// 9-bit adder
+  
+  // 9th bit module
+  bit_9 bit_9_unit();
+
+  control control_unit
+  		(
+			.Clk(clk),
+			.Reset(reset_s),
+			.Run(run_s),
+			.clra_ldb(cleara_loadb_s),
+      .b0(b[0]),
+      .OutCleara(cleara),
+      .OutLoadb(loadb),
+      .OutShift(shift),
+      .OutAdd(add),
+      .OutSum(sum)
+      );
+
+	HexDriver HexAL (.In0(a[3:0]),.Out0(AhexL));
+	HexDriver HexBL (.In0(b[3:0]),.Out0(BhexL));
+	HexDriver HexAU (.In0(a[7:4]),.Out0(AhexU));
+  HexDriver HexBU (.In0(b[7:4]),.Out0(BhexU));
+
+  sync buttons_sync[2:0] (clk,{~run,~reset,~cleara_loadb},{run_s,reset_s,cleara_loadb_s});
+  sync s_sync[7:0] (clk,s,s_s)
+
+endmodule
