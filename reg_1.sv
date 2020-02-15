ffb796b2d8a1b76ffa22a2b92c886cf7197bdc5e
Aki-Zakura <46938051+Aki-Zakura@users.noreply.github.com>
2020-02-15 13:28:34 -0600

    1-bit register created.

---
+++ reg_1.sv
@@ -0,0 +1,13 @@
+module reg_1 (input  logic clk, reset, load,
+              input  logic din,
+              output logic dout);
+
+    always_ff @ (posedge clk)
+    begin
+        if (reset)
+            dout <= 1'h0;
+        else if (load)
+            dout <= din;
+    end
+
+endmodule
