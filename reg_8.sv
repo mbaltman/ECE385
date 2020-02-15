02f57f6c2f532b69bd0aebb988436c806e0b1bb9
mbaltman <mbaltman@cps.edu>
2020-02-15 10:06:58 -0600

    Update reg_8.sv

--- reg_8.sv
+++ reg_8.sv
@@ -1,22 +1,22 @@
-module reg_8 (input  logic clk, reset, Shift_In, Load, Shift_En,
-              input  logic [7:0]  D,
-              output logic Shift_Out,
-              output logic [7:0]  Data_Out);
+module reg_8 (input  logic clk, reset, shift_in, load, shift_en,
+              input  logic [7:0]  din,
+              output logic shift_out,
+              output logic [7:0]  dout);
 
     always_ff @ (posedge Clk)
     begin
-	 	 if (Reset) //notice, this is a sycnrhonous reset, which is recommended on the FPGA
-			  Data_Out <= 8'h0;
-		 else if (Load)
+	 	 if (reset) //notice, this is a sycnrhonous reset, which is recommended on the FPGA
+			  dout <= 8'h0;
+		 else if (load)
 			  Data_Out <= D;
-		 else if (Shift_En)
+		 else if (shift_en)
 		 begin
 			  //concatenate shifted in data to the previous left-most 3 bits
 			  //note this works because we are in always_ff procedure block
-			  Data_Out <= { Shift_In, Data_Out[7:1] }; 
+			  dout <= { shift_in, dout[7:1] }; 
 	    end
     end
 	
-    assign Shift_Out = Data_Out[0];
+    assign shift_out = dout[0];
 
 endmodule
