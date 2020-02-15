6dacd240bed9de19299f046d058ec00dcd7391bf
mbaltman <mbaltman@cps.edu>
2020-02-15 09:50:11 -0600

    changes in processor and reg

--- Processor.sv
+++ Processor.sv
@@ -1,6 +1,6 @@
 /* This module is the processor for multiplier, and it does all the wiring and instatiation. */
 module processor
-	(
+	
   (
   input logic clk, reset, run, cleara_loadb,
   input logic[7:0] s,
@@ -8,7 +8,7 @@ module processor
   output logic[7:0] aval, bval,
   output logic x
 	);
-  );
+  
 
 /* Intermediate logic variables go here. */
   logic shift, add, sub, cleara, loadb;
@@ -18,9 +18,6 @@ module processor
 
 /* Instantiation of other modules here. */
   // two 8-bit shift registers
-  reg_8 reg_a(.clk_reg(clk),.reset_reg(reset|cleara),.shift_in_reg(x),.shift_en_reg(shift),.load_reg(add|sub),.din_reg(?),.shift_out_reg(between_reg),.dout_reg(a));
-  reg_8 reg_b(.clk_reg(clk),.reset_reg(),.shift_in_reg(between_reg),.shift_en_reg(shift),.load_reg(loadb),.din_reg(s_s),.shift_out_reg(),.dout_reg(b));
-
 	// 9-bit adder
   reg_8 reg_a(.clk(clk),.reset(reset|cleara),.shift_in(x),.shift_en(shift),.load(add|sub),.din(?),.shift_out(between),.dout(a));
   reg_8 reg_b(.clk(clk),.reset(),.shift_in(between),.shift_en(shift),.load(loadb),.din(s_s),.shift_out(),.dout(b));
@@ -32,22 +29,6 @@ module processor
   bit_9 bit_9_unit();
 
   control control_unit
-  		(
-			.Clk(clk),
-			.Reset(reset_s),
-			.Run(run_s),
-			.clra_ldb(cleara_loadb_s),
-      .b0(b[0]),
-      .OutCleara(cleara),
-      .OutLoadb(loadb),
-      .OutShift(shift),
-      .OutAdd(add),
-      .OutSub(sub)
-      );
-
-	HexDriver HexAL (.In0(a[3:0]),.Out0(AhexL));
-	HexDriver HexBL (.In0(b[3:0]),.Out0(BhexL));
-	HexDriver HexAU (.In0(a[7:4]),.Out0(AhexU));
   (
   .clk(clk),
   .reset(reset_s),
@@ -69,4 +50,4 @@ module processor
   sync buttons_sync[2:0] (clk,{~run,~reset,~cleara_loadb},{run_s,reset_s,cleara_loadb_s});
   sync s_sync[7:0] (clk,s,s_s)
 
-endmoduleendmodule
+endmodule
