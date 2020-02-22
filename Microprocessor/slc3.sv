52119e4511293d074e6c98e647aa3824858d29ab
mbaltman <mbaltman@cps.edu>
2020-02-22 13:03:10 -0600

    more outputs
    read the value of pc and mar

--- Microprocessor/slc3.sv
+++ Microprocessor/slc3.sv
@@ -5,7 +5,7 @@ module slc3(
 	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7,
 	output logic CE, UB, LB, OE, WE,
 	output logic [19:0] ADDR,
-	output logic [15:0] IR,
+	output logic [15:0] IR,PC,MAR,
 	output logic GatePC, LD_MAR, LD_PC, MIO_EN, Gate_MDR, LD_IR,
 	output logic [1:0] PCMUX,
 	inout wire [15:0] Data // tristate buffers need to be of type wire
@@ -28,7 +28,7 @@ module slc3(
 
 
 	logic [15:0] MDR_In;
-	logic [15:0] MAR, MDR, PC, MARMUX, ADDADD, ALUOUT;
+	logic [15:0] MDR, MARMUX, ADDADD, ALUOUT;
 	logic [15:0] Data_from_SRAM, Data_to_SRAM;
 
 	// Signals being displayed on hex display
