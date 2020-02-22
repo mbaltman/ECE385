040f03b06224da905bbfb0285ac10d5178b4c28b
Aki-Zakura <46938051+Aki-Zakura@users.noreply.github.com>
2020-02-22 09:50:51 -0600

    Create MDR_module.sv

---
+++ Microprocessor/MDR_module.sv
@@ -0,0 +1,17 @@
+module MDR_module (
+    input  logic [15:0] data, mdrin,
+    input  logic mioen, ld, clk, reset,
+    output logic [15:0] mdrout);
+
+    logic [15:0] mdr_interm;
+    mux2 miomux (.d0(data), .d1(mdrin), .s(mioen), .y(mdr_interm));
+
+    always_ff @ (posedge clk)
+    begin
+        if (reset)
+            mdrout <= 16'h0;
+        else if (ld)
+            mdrout <= mdr_interm;
+    end
+
+endmodule
