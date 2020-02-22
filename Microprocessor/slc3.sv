8b4236ab1fa00a042123659371f80fbcfd3ade28
mbaltman <mbaltman@cps.edu>
2020-02-22 10:07:43 -0600

    started testbench

--- Microprocessor/slc3.sv
+++ Microprocessor/slc3.sv
@@ -5,7 +5,7 @@ module slc3(
 	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7,
 	output logic CE, UB, LB, OE, WE,
 	output logic [19:0] ADDR,
-	inout wire [15:0] Data // tristate buffers need to be of type wire
+	inout wire [15:0] Data,IR // tristate buffers need to be of type wire
 	);
 
 	// Declaration of push button active high signals
