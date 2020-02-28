aeba7b278d9a760b3581d9bf27d80cc54fbb9ba2
Aki-Zakura <46938051+Aki-Zakura@users.noreply.github.com>
2020-02-28 16:00:34 -0600

    Clean up.

--- Microprocessor/alu_module.sv
+++ Microprocessor/alu_module.sv
@@ -2,17 +2,17 @@ module alu_module(
     input  logic [15:0] A, B, // B comes from the output of sr2mux_module
     input  logic [1:0]  s,
     output logic [15:0] out);
-	 
+
     always_comb
-	 begin
+	begin
     unique case(s)
-         2'b00:
+        2'b00:
             out = A + B;
-         2'b01:
+        2'b01:
             out = A & B;
-         2'b10:
+        2'b10:
             out = ~A;
-         2'b11:
+        2'b11:
             out = A;
 	 endcase
     end
