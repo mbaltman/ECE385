44d310b6b06c37a5aea4370289e9b191b792e1f8
Aki-Zakura <46938051+Aki-Zakura@users.noreply.github.com>
2020-02-29 01:27:37 -0600

    States in numerical order.

--- Microprocessor/ISDU.sv
+++ Microprocessor/ISDU.sv
@@ -98,16 +98,6 @@ module ISDU(
 			Halted :
 				if (Run)
 					Next_state = S_18;
-			S_18 :
-				Next_state = S_33_1;
-			// Any states involving SRAM require more than one clock cycles.
-			// The exact number will be discussed in lecture.
-			S_33_1 :
-				Next_state = S_33_2;
-			S_33_2 :
-				Next_state = S_35;
-			S_35 :
-				Next_state = S_32;
 
 			PauseIR1 :
 				if (~Continue)
@@ -119,6 +109,45 @@ module ISDU(
 					Next_state = PauseIR2;
 				else
 					Next_state = S_18;
+
+			S_00 :
+				begin
+					if(BEN)
+						Next_state = S_22;
+					else
+						Next_state = S_18;
+				end
+
+			S_01 :
+				Next_state = S_18;
+
+			S_05 :
+				Next_state = S_18;
+
+			S_06 :
+				Next_state = S_25_1;
+
+			S_07 :
+				Next_state = S_23;
+
+			S_16_1 :
+				Next_state = S_16_2;
+			S_16_2 :
+				Next_state = S_18;
+
+			S_18 :
+				Next_state = S_33_1;
+
+			S_23 :
+				Next_state = S_16_1;
+
+			S_25_1 :
+				Next_state = S_25_2;
+			S_25_2 :
+				Next_state = S_27;
+
+			S_27 :
+				Next_state = S_18;
 
 			S_32 :
 				case (Opcode)
@@ -136,43 +165,13 @@ module ISDU(
 						Next_state = S_18;
 				endcase
 
-			S_01 :
-				Next_state = S_18;
+			S_33_1 :
+				Next_state = S_33_2;
+			S_33_2 :
+				Next_state = S_35;
 
-			S_06 :
-				Next_state = S_25_1;
-
-			S_25_1 :
-				Next_state = S_25_2;
-
-			S_25_2:
-				Next_state = S_27;
-
-			S_27 :
-				Next_state = S_18;
-
-			S_07:
-				Next_state = S_23;
-
-			S_23:
-				Next_state = S_16_1;
-
-			S_16_1:
-				Next_state = S_16_2;
-
-			S_16_2:
-				Next_state = S_18;
-
-			S_05:
-				Next_state = S_18;
-
-			S_00:
-				begin
-					if(BEN)
-						Next_state = S_22;
-					else
-						Next_state = S_18;
-				end
+			S_35 :
+				Next_state = S_32;
 
 			default: ;
 		endcase
@@ -181,34 +180,8 @@ module ISDU(
 		case (State)
 			Halted: ;
 
-			S_18 :
-				begin
-					GatePC = 1'b1;
-					LD_MAR = 1'b1;
-					PCMUX = 2'b00;
-					LD_PC = 1'b1;
-				end
-
-			S_33_1 :
-				Mem_OE = 1'b0;
-
-			S_33_2 :
-				begin
-					Mem_OE = 1'b0;
-					LD_MDR = 1'b1;
-				end
-
-			S_35 :
-				begin
-					GateMDR = 1'b1;
-					LD_IR = 1'b1;
-				end
-
 			PauseIR1: ;
 			PauseIR2: ;
-
-			S_32 :
-				LD_BEN = 1'b1;
 
 			S_01 :
 				begin
@@ -218,6 +191,16 @@ module ISDU(
 					LD_REG = 1'b1;
 				end
 
+			S_05 :
+				begin
+					SR1MUX = 1'b1;
+					ALUK = 2'b01;
+					GateALU = 1'b1;
+					LD_REG =1'b1;
+					DRMUX = 1'b0;
+					LD_CC = 1'b1;
+				end
+
 			S_06, S_07 :
 				begin
 					LD_MAR = 1'b1;
@@ -225,6 +208,36 @@ module ISDU(
 					ADDR1MUX = 1'b1;
 					ADDR2MUX = 2'b01;
 					GateMARMUX = 1'b1;
+				end
+
+			S_16_1, S_16_2 :
+				begin
+					Mem_WE = 1'b0;
+				end
+
+			S_18 :
+				begin
+					GatePC = 1'b1;
+					LD_MAR = 1'b1;
+					PCMUX = 2'b00;
+					LD_PC = 1'b1;
+				end
+
+			S_22:
+				begin
+					LD_PC =1'b1;
+					ADDR2MUX = 2'b10;
+					ADDR1MUX = 1'b0;
+					PCMUX = 2'b10;
+				end
+
+			S_23 :
+				begin
+					SR1MUX =1'b0;
+					ALUK = 2'b11;
+					GateALU = 1'b1;
+					LD_MDR = 1'b1;
+					Mem_OE = 1'b1;
 				end
 
 			S_25_1, S_25_2 :
@@ -241,36 +254,21 @@ module ISDU(
 					LD_REG = 1'b1;
 				end
 
-			S_23 :
+			S_32 :
+				LD_BEN = 1'b1;
+
+			S_33_1 :
+				Mem_OE = 1'b0;
+			S_33_2 :
 				begin
-					SR1MUX =1'b0;
-					ALUK = 2'b11;
-					GateALU = 1'b1;
+					Mem_OE = 1'b0;
 					LD_MDR = 1'b1;
-					Mem_OE = 1'b1;
 				end
 
-			S_16_1, S_16_2 :
+			S_35 :
 				begin
-					Mem_WE = 1'b0;
-				end
-
-			S_05:
-				begin
-					SR1MUX = 1'b1;
-					ALUK = 2'b01;
-					GateALU = 1'b1;
-					LD_REG =1'b1;
-					DRMUX = 1'b0;
-					LD_CC = 1'b1;
-				end
-
-			S_22:
-				begin
-				 LD_PC =1'b1;
-				 ADDR2MUX = 2'b10;
-				 ADDR1MUX = 1'b0;
-				 PCMUX = 2'b10;
+					GateMDR = 1'b1;
+					LD_IR = 1'b1;
 				end
 
 			default: ;
