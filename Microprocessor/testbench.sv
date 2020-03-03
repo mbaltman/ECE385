3e2ecbeb51a8aaa4f38eb79a7ffe0ddc4c9572bd
Aki-Zakura <46938051+Aki-Zakura@users.noreply.github.com>
2020-03-02 19:43:03 -0600

    Update testbench.sv

--- Microprocessor/testbench.sv
+++ Microprocessor/testbench.sv
@@ -34,7 +34,7 @@ module testbench();
 		Run = 1;
 		Continue = 1;
 
-		S = 16'h005A;
+		S = 16'h0003;
 
 		#2 Reset = 0;
 		#2 Reset = 1;
@@ -44,84 +44,5 @@ module testbench();
 		#4 Run = 0;
 		#4 Run = 1;
 
-		#250 S = 16'h0002;
-		#4 Continue = 0;
-		#200 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-   
-	   #4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#250 S = 16'h0003;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		
-		
-		
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-   
-	   #4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		#4 Continue = 0;
-		#150 Continue = 1;
-		
-		
 	end
 endmodule
