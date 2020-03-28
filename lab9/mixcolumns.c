cc1b520abfdae2e61be824893ee7aaaa3a428b5b
Aki-Zakura <46938051+Aki-Zakura@users.noreply.github.com>
2020-03-28 17:39:39 -0500

    Mixcolums and shiftrows.

---
+++ lab9/mixcolumns.c
@@ -0,0 +1,31 @@
+char * MixColumns(char * currstate)
+{
+    char newstate[16];
+    for(int i = 0; i < 4; i++) // column number 0~3
+    {
+        char a0 = currstate[0 + i];
+        char a1 = currstate[4 + i];
+        char a2 = currstate[8 + i];
+        char a3 = currstate[12 + i];
+
+        char b0 = xtime(a0) ^ (xtime(a1)^a1) ^ (a2) ^ (a3);
+        char b1 = (a0) ^ (xtime(a1)) ^ (xtime(a2)^a2) ^ (a3);
+        char b2 = (a0) ^ (a1) ^ (xtime(a2)) ^ (xtime(a3)^a3);
+        char b3 = (xtime(a0)^a0) ^ (a1) ^ (a2) ^ (xtime(a3));
+
+        newstate[0 + i] = b0;
+        newstate[4 + i] = b1;
+        newstate[8 + i] = b2;
+        newstate[12 + i] = b3;
+    }
+    return newstate;
+}
+
+char xtime(char a)
+{
+    char a_out;
+    char comp_1b = charsToHex('1', 'b'); // define {1b} character in HEX
+    if (a < 0) a_out = (a << 1) ^ comp_1b; // left shift by 1 bit and XOR {1b}
+    else a_out = a << 1; // left shift only
+    return a_out;
+}
---
+++ lab9/shiftrows.c
@@ -0,0 +1,24 @@
+char * ShiftRows(char * currstate)
+{
+    char newstate[16];
+    newstate[0] = currstate[0];
+    newstate[1] = currstate[1];
+    newstate[2] = currstate[2];
+    newstate[3] = currstate[3];
+
+    newstate[4] = currstate[5];
+    newstate[5] = currstate[6];
+    newstate[6] = currstate[7];
+    newstate[7] = currstate[4];
+
+    newstate[8] = currstate[10];
+    newstate[9] = currstate[11];
+    newstate[10] = currstate[8];
+    newstate[11] = currstate[9];
+
+    newstate[12] = currstate[15];
+    newstate[13] = currstate[12];
+    newstate[14] = currstate[13];
+    newstate[15] = currstate[14];
+    return newstate;
+}
