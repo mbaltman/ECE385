b39de58bf0375c21e22c68a53d256f5dc8d90ce6
mbaltman <mbaltman@cps.edu>
2020-05-03 13:07:01 -0500

    subtract 80 save blocks

--- final_project/savedblocks.sv
+++ final_project/savedblocks.sv
@@ -8,16 +8,13 @@ module savedblocks (
     );
 
     logic [7:0] topleft_x, topleft_y, i;
-<<<<<<< HEAD
+
 	 logic [7:0] baseindex;
 	 logic shiftDown,shiftDown_in;
-=======
-    logic [7:0] baseindex;
->>>>>>> be2f4fa459d39acb9aebdcb56e0faaf587ddb408
 
     always_comb
     begin
-        topleft_x = Block_X_Pos / 10'd20; // ranging from 0 to 9
+        topleft_x = (Block_X_Pos- 10'd80) / 10'd20; // ranging from 0 to 9
         topleft_y = Block_Y_Pos / 10'd20; // ranging from 0 to 23
     end
 
@@ -33,7 +30,6 @@ module savedblocks (
         end
         else if (saveenable)
         begin
-<<<<<<< HEAD
 				baseindex = topleft_y * 8'd10;
 				
 				if(inputstream[0])
@@ -83,60 +79,7 @@ module savedblocks (
 				
 				if(inputstream[15])	
 					state_output[baseindex + 8'd30 + topleft_x + 8'd3] <= inputstream[15] ;
-				
-=======
-            baseindex = topleft_y * 8'd10;
 
-            if(inputstream[0])
-                state_output[baseindex + topleft_x + 8'd0] <= inputstream[0];
-
-            if(inputstream[1])
-                state_output[baseindex + topleft_x + 8'd1] <= inputstream[1];
-
-            if(inputstream[2])
-                state_output[baseindex + topleft_x + 8'd2] <= inputstream[2];
-
-            if(inputstream[3])
-                state_output[baseindex + topleft_x + 8'd3] <= inputstream[3];
-
-            if(inputstream[4])
-                state_output[baseindex + 8'd10 + topleft_x + 8'd0] <= inputstream[4];
-
-            if(inputstream[5])
-                state_output[baseindex + 8'd10 + topleft_x + 8'd1] <= inputstream[5];
-
-            if(inputstream[6])
-                state_output[baseindex + 8'd10 + topleft_x + 8'd2] <= inputstream[6];
-
-            if(inputstream[7])
-                state_output[baseindex + 8'd10 + topleft_x + 8'd3] <= inputstream[7];
-
-            if(inputstream[8])
-                state_output[baseindex + 8'd20 + topleft_x + 8'd0] <= inputstream[8];
-
-            if(inputstream[9])
-                state_output[baseindex + 8'd20 + topleft_x + 8'd1] <= inputstream[9] ;
-
-            if(inputstream[10])
-                state_output[baseindex + 8'd20 + topleft_x + 8'd2] <= inputstream[10];
-
-            if(inputstream[11])
-                state_output[baseindex + 8'd20 + topleft_x + 8'd3] <= inputstream[11];
-
-            if(inputstream[12])
-                state_output[baseindex + 8'd30 + topleft_x + 8'd0] <= inputstream[12];
-
-            if(inputstream[13])
-                state_output[baseindex + 8'd30 + topleft_x + 8'd1] <= inputstream[13];
-
-            if(inputstream[14])
-                state_output[baseindex + 8'd30 + topleft_x + 8'd2] <= inputstream[14];
-
-            if(inputstream[15])
-                state_output[baseindex + 8'd30 + topleft_x + 8'd3] <= inputstream[15];
-
-
->>>>>>> be2f4fa459d39acb9aebdcb56e0faaf587ddb408
         end
 		  else
 		  begin
